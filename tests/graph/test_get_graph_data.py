3fb928c911c39019648da67bf3452845fca67e50
Osogi <dragonmail135159@gmail.com>
2024-09-05 14:51:12 +0300

    test: add tests for task1

---
+++ tests/graph/test_get_graph_data.py
@@ -0,0 +1,19 @@
+import pytest
+from project.graph import get_graph_data
+
+
+def test_bzip_graph():
+    expected_nodes_count = 632
+    expected_edges_count = 556
+    expected_labels = {"d", "a"}
+
+    dt = get_graph_data("bzip")
+
+    assert dt.nodes_count == expected_nodes_count
+    assert dt.edges_count == expected_edges_count
+    assert set(dt.labels) == expected_labels
+
+
+def test_nonexist_graph():
+    with pytest.raises(FileNotFoundError):
+        get_graph_data("pikapikachu")
---
+++ tests/graph/test_save_labeled_two_cycles_graph.py
@@ -0,0 +1,29 @@
+import pytest
+import cfpq_data
+from pathlib import Path
+import networkx
+import pydot
+from project.graph import save_labeled_two_cycles_graph
+
+
+def test_saving(tmpdir):
+    save_path = Path(tmpdir).joinpath("graph.dot")
+    n = 13
+    m = 42
+    labels = ("aboba", "answer")
+
+    expected_nodes_count = n + m + 1
+    expected_labels = set(labels)
+
+    save_labeled_two_cycles_graph(n, m, labels, save_path)
+
+    load_graphs = pydot.graph_from_dot_file(save_path)
+
+    if load_graphs is None:
+        pytest.fail("Can't laod saved graph")
+
+    load_graph = load_graphs[0]
+    nx_graph: networkx.MultiDiGraph = networkx.drawing.nx_pydot.from_pydot(load_graph)
+
+    assert nx_graph.number_of_nodes() == expected_nodes_count
+    assert set(cfpq_data.get_sorted_labels(nx_graph)) == expected_labels
