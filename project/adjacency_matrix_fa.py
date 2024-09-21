30c320c59c17b439b346564f1a5c47bfc5bcd350
Osogi <dragonmail135159@gmail.com>
2024-09-21 21:39:10 +0300

    feat: add methods to accept words and check for emptiness for `AdjacencyMatrixFA` class

--- project/adjacency_matrix_fa.py
+++ project/adjacency_matrix_fa.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Set, Tuple
+from typing import Dict, Iterable, List, Set, Tuple
+import numpy
 from pyformlang.finite_automaton import NondeterministicFiniteAutomaton, State, Symbol
 from scipy import sparse
 
@@ -40,3 +41,54 @@ class AdjacencyMatrixFA:
                 shape=(self.states_count, self.states_count),
                 dtype=bool,
             )
+
+    def create_start_vector(self) -> sparse.csc_array:
+        st_ind = [self.state_to_index[st] for st in self.st_states]
+        data = [True] * len(st_ind)
+        zeroes = [0] * len(st_ind)
+
+        vec = sparse.csc_array(
+            (data, (zeroes, st_ind)),
+            shape=(1, self.states_count),
+            dtype=bool,
+        )
+
+        return vec
+
+    def accepts(self, word: Iterable[Symbol]) -> bool:
+        vec = self.create_start_vector()
+
+        for symb in word:
+            matrix = self.bool_decomp.get(symb)
+            if matrix is not None:
+                vec = vec @ matrix
+            else:
+                return False
+
+        for fin_st in self.fin_states:
+            ind = self.state_to_index[fin_st]
+            if vec[0, ind]:
+                return True
+
+        return False
+
+    def is_empty(self) -> bool:
+        matrix: numpy.ndarray | None = None
+
+        for symb in self.bool_decomp.keys():
+            if matrix is None:
+                matrix = self.bool_decomp[symb].toarray()
+            else:
+                matrix += self.bool_decomp[symb]
+        if matrix is None:
+            return True
+
+        for _ in range(self.states_count):
+            matrix += matrix @ matrix
+
+        for start in self.st_states:
+            for fin in self.fin_states:
+                if matrix[self.state_to_index[start], self.state_to_index[fin]]:
+                    return False
+
+        return True
