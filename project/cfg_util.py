f4b8a995eb0973ff8dec68ac3db10fcb511fd0da
Osogi <dragonmail135159@gmail.com>
2024-10-28 18:24:29 +0300

    refactor: separate common cfg solevers method

--- project/cfg_util.py
+++ project/cfg_util.py
@@ -8,6 +8,10 @@ import networkx as nx
 from project.adjacency_matrix_fa import AdjacencyMatrixFA
 from project.graph_util import graph_to_nfa
 
+LABEL_NAME = "label"
+var_t: TypeAlias = pycfg.Variable
+term_t: TypeAlias = pycfg.Terminal
+
 
 def cfg_to_weak_normal_form(cfg: pycfg.CFG) -> pycfg.CFG:
     epsil_prod = cfg.get_nullable_symbols()
@@ -22,11 +26,66 @@ def cfg_to_weak_normal_form(cfg: pycfg.CFG) -> pycfg.CFG:
     return pycfg.CFG(start_symbol=cfg.start_symbol, productions=cfg_prod)
 
 
-def matrix_cfpq_solver_scope():
-    label_name = "label"
+class CFGSolverTemaplate:
+    @staticmethod
+    def add_to_dict(dict, key, val):
+        ts = dict.get(key, set())
+        ts.add(val)
+        dict[key] = ts
 
-    var_t: TypeAlias = pycfg.Variable
-    term_t: TypeAlias = pycfg.Terminal
+    def rebuild_graph(self, graph: nx.DiGraph) -> nx.MultiDiGraph:
+        nodes = graph.nodes()
+        edges = graph.edges(data=True)
+        new_edges = []
+
+        for edg in edges:
+            d: Dict = edg[2]
+            label_val = term_t(d.get(LABEL_NAME))
+
+            var_set = self.terminal2Vars.get(label_val, set())
+            for var in var_set:
+                new_edges.append((edg[0], edg[1], {LABEL_NAME: var}))
+
+        for var in self.epsilonVars:
+            for node in nodes:
+                new_edges.append((node, node, {LABEL_NAME: var}))
+
+        res_graph = nx.MultiDiGraph(new_edges)
+        for node in nodes:
+            res_graph.add_node(node)
+
+        return res_graph
+
+    def process_cfg(self, cfg: pycfg.CFG) -> pycfg.CFG:
+        cfg = cfg_to_weak_normal_form(cfg)
+
+        for prod in cfg.productions:
+            head = prod.head
+            b = prod.body
+            if len(b) == 1:
+                if b[0] == pycfg.Epsilon():
+                    self.epsilonVars.add(head)
+                else:
+                    self.add_to_dict(self.terminal2Vars, b[0], head)
+            elif len(b) == 2:
+                self.add_to_dict(self.vars2Vars, (b[0], b[1]), head)
+            else:
+                raise ValueError("Get normal cfg with len of product > 2")
+
+        return cfg
+
+    def __init__(self, cfg: pycfg.CFG, graph: nx.DiGraph):
+        # N_i -> epsilon
+        self.epsilonVars: Set[var_t] = set()
+
+        # N_i -> t_j
+        self.terminal2Vars: Dict[term_t, Set[var_t]] = {}
+
+        # N_i -> N_j N_k
+        self.vars2Vars: Dict[Tuple[var_t, var_t], Set[var_t]] = {}
+
+
+def matrix_cfpq_solver_scope():
     dict_ind_t: TypeAlias = var_t | Symbol
 
     @dataclass(frozen=True, eq=True)
@@ -35,71 +94,32 @@ def matrix_cfpq_solver_scope():
         b1: var_t
         b2: var_t
 
-    class MatrixCFPQSolver:
-        def rebuild_graph(self, graph: nx.DiGraph) -> nx.MultiDiGraph:
-            nodes = graph.nodes()
-            edges = graph.edges(data=True)
-            new_edges = []
-
-            for edg in edges:
-                d: Dict = edg[2]
-                label_val = term_t(d.get(label_name))
-
-                var_set = self.terminal2Vars.get(label_val, set())
-                for var in var_set:
-                    new_edges.append((edg[0], edg[1], {label_name: var}))
-
-            for var in self.epsilonVars:
-                for node in nodes:
-                    new_edges.append((node, node, {label_name: var}))
-
-            res_graph = nx.MultiDiGraph(new_edges)
-            for node in nodes:
-                res_graph.add_node(node)
-
-            return res_graph
-
-        def _process_cfg(self, cfg: pycfg.CFG) -> pycfg.CFG:
-            cfg = cfg_to_weak_normal_form(cfg)
-
-            def add_to_dict(dict, key, val):
-                ts = dict.get(key, set())
-                ts.add(val)
-                dict[key] = ts
-
-            for prod in cfg.productions:
-                head = prod.head
-                b = prod.body
-                if len(b) == 1:
-                    if b[0] == pycfg.Epsilon():
-                        self.epsilonVars.add(head)
-                    else:
-                        add_to_dict(self.terminal2Vars, b[0], head)
-                elif len(b) == 2:
-                    add_to_dict(
-                        self.var2dependent, b[0], ProductionCase(head, b[0], b[1])
-                    )
-                    add_to_dict(
-                        self.var2dependent, b[1], ProductionCase(head, b[0], b[1])
-                    )
-                else:
-                    raise ValueError("Get normal cfg with len of product > 2")
-
-            return cfg
-
+    class MatrixCFPQSolver(CFGSolverTemaplate):
         def __init__(self, cfg: pycfg.CFG, graph: nx.DiGraph):
-            # N_i -> epsilon
-            self.epsilonVars: Set[var_t] = set()
-
-            # N_i -> t_j
-            self.terminal2Vars: Dict[term_t, Set[var_t]] = {}
+            super().__init__(cfg, graph)
 
             # N_i -> N_j N_k
             # (N_i, N_k) \in var2dependent[N_j]
             # (N_i, N_j) \in var2dependent[N_k]
             self.var2dependent: Dict[dict_ind_t, Set[ProductionCase]] = {}
 
-            cfg = self._process_cfg(cfg)
+            cfg = self.process_cfg(cfg)
+            self.start_var = cfg.start_symbol
+
+            for body in self.vars2Vars:
+                heads = self.vars2Vars[body]
+                for head in heads:
+                    self.add_to_dict(
+                        self.var2dependent,
+                        body[0],
+                        ProductionCase(head, body[0], body[1]),
+                    )
+                    self.add_to_dict(
+                        self.var2dependent,
+                        body[1],
+                        ProductionCase(head, body[0], body[1]),
+                    )
+
             new_graph = self.rebuild_graph(graph)
             nfa = graph_to_nfa(new_graph)
 
@@ -110,8 +130,6 @@ def matrix_cfpq_solver_scope():
             for v in cfg.variables:
                 if self.bool_decomp.get(v, None) is None:
                     self.bool_decomp[v] = csc_array(shape, dtype=bool)
-
-            self.start_var = cfg.start_symbol
 
         def step(self, pc: ProductionCase) -> bool:
             m1 = self.bool_decomp[pc.b1]
