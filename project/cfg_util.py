0d9013fcc53da0e0f7a01b390fa3e2edf3729f29
Osogi <dragonmail135159@gmail.com>
2024-11-11 18:07:52 +0300

    feat: finsh gll cfpq

--- project/cfg_util.py
+++ project/cfg_util.py
@@ -381,15 +381,15 @@ def tensor_based_cfpq(
     return res
 
 
-@dataclass
+@dataclass(frozen=True)
 class RsmState:
     var: Symbol
     sub_state: str
 
 
-@dataclass
+@dataclass(frozen=True)
 class SPPFNode:
-    gss: GSSNode
+    gssn: GSSNode
     state: RsmState
     node: int
 
@@ -432,15 +432,33 @@ class GSSNode:
         return res_set
 
 
+class GSStack:
+    body: Dict[Tuple[RsmState, int], GSSNode]
+
+    def __init__(self):
+        self.body = {}
+
+    def get_node(self, rsm_st: RsmState, node: int):
+        res = self.body.get((rsm_st, node), None)
+        if res is None:
+            res = GSSNode(rsm_st, node)
+            self.body[(rsm_st, node)] = res
+        return res
+
+
 @dataclass
-class StateEdges:
+class RsmStateData:
     term_edges: Dict[Symbol, RsmState]
-    var_edges: Dict[Symbol, RsmState]
+    var_edges: Dict[Symbol, Tuple[RsmState, RsmState]]
+    # first RsmState start-state of symbol
+    # second RsmState next stete after var (return state)
+
+    is_final: bool
 
 
 class GllCFPQSolver:
     def is_term(self, s: str) -> bool:
-        return Symbol(s) not in self.state2edges
+        return Symbol(s) not in self.rsmstate2data
 
     def init_graph_data(self, graph: nx.DiGraph):
         # Init data for graph traverse
@@ -459,27 +477,36 @@ class GllCFPQSolver:
     def init_rsm_data(self, rsm: rsa.RecursiveAutomaton):
         # Init data for RSM traverse
         for var in rsm.boxes:
-            self.state2edges[var] = {}
+            self.rsmstate2data[var] = {}
 
         for var in rsm.boxes:
             box = rsm.boxes[var]
             fa: DeterministicFiniteAutomaton = box.dfa
             gbox = fa.to_networkx()
 
-            sub_dict = self.state2edges[var]
+            sub_dict = self.rsmstate2data[var]
+
+            for sub_state in gbox.nodes:
+                is_fin = sub_state in fa.final_states
+                sub_dict[sub_state] = RsmStateData({}, {}, is_fin)
 
             edges = gbox.edges(data=LABEL_NAME)
             for from_st, to_st, symb in edges:
                 if symb is not None:
-                    st_edges: StateEdges = sub_dict.get(from_st, StateEdges({}, {}))
+                    st_edges = sub_dict[from_st]
                     if self.is_term(symb):
                         st_edges.term_edges[symb] = RsmState(var, to_st)
                     else:
                         bfa: DeterministicFiniteAutomaton = rsm.boxes[Symbol(symb)].dfa
                         box_start = bfa.start_state.value
-                        st_edges.var_edges[symb] = RsmState(Symbol(symb), box_start)
+                        st_edges.var_edges[symb] = (
+                            RsmState(Symbol(symb), box_start),
+                            RsmState(var, to_st),
+                        )
 
-                    sub_dict[from_st] = st_edges
+        start_symb = rsm.initial_label
+        start_fa: DeterministicFiniteAutomaton = rsm.boxes[start_symb].dfa
+        self.start_rstate = RsmState(start_symb, start_fa.start_state.value)
 
     def __init__(
         self,
@@ -487,10 +514,118 @@ class GllCFPQSolver:
         graph: nx.DiGraph,
     ):
         self.nodes2edges: Dict[int, Dict[Symbol, Set[int]]] = {}
-        self.state2edges: Dict[Symbol, Dict[str, StateEdges]] = {}
+        self.rsmstate2data: Dict[Symbol, Dict[str, RsmStateData]] = {}
+        self.start_rstate: RsmState
+
+        self.rsm = rsm
+        self.graph = graph
 
         self.init_graph_data(graph)
         self.init_rsm_data(rsm)
+
+        self.gss = GSStack()
+        self.accept_gssnode = self.gss.get_node(RsmState(Symbol("$"), "fin"), -1)
+
+        self.unprocessed: Set[SPPFNode] = set()
+        self.added: Set[SPPFNode] = set()
+
+    def add_sppf_nodes(self, snodes: Set[SPPFNode]):
+        snodes.difference_update(self.added)
+
+        self.added.update(snodes)
+        self.unprocessed.update(snodes)
+
+    def filter_poped_nodes(
+        self, snodes: Set[SPPFNode], prev_snode: SPPFNode
+    ) -> Tuple[Set[SPPFNode], Set[Tuple[int, int]]]:
+        node_res_set = set()
+        start_fin_res_set = set()
+
+        for sn in snodes:
+            if sn.gssn == self.accept_gssnode:
+                start_node = prev_snode.gssn.node
+                fin_node = sn.node
+                start_fin_res_set.add((start_node, fin_node))
+            else:
+                node_res_set.add(sn)
+
+        return (node_res_set, start_fin_res_set)
+
+    def step(self, sppfnode: SPPFNode) -> Set[Tuple[int, int]]:
+        rsm_st = sppfnode.state
+        rsm_dat = self.rsmstate2data[rsm_st.var][rsm_st.sub_state]
+        # print(f"RSM dat: {rsm_dat}")
+
+        def term_step():
+            rsm_terms = rsm_dat.term_edges
+            graph_terms = self.nodes2edges[sppfnode.node]
+            for term in rsm_terms:
+                if term in graph_terms:
+                    new_sppf_nodes = set()
+                    rsm_new_st = rsm_terms[term]
+                    graph_new_nodes = graph_terms[term]
+                    for gn in graph_new_nodes:
+                        new_sppf_nodes.add(SPPFNode(sppfnode.gssn, rsm_new_st, gn))
+
+                    self.add_sppf_nodes(new_sppf_nodes)
+
+        def var_step() -> Set[Tuple[int, int]]:
+            start_fin_set = set()
+            for var in rsm_dat.var_edges:
+                var_start_rsm_st, ret_rsm_st = rsm_dat.var_edges[var]
+
+                inner_gss_node = self.gss.get_node(var_start_rsm_st, sppfnode.node)
+                post_pop_sppf_nodes = inner_gss_node.add_edge(ret_rsm_st, sppfnode.gssn)
+
+                post_pop_sppf_nodes, sub_start_fin_set = self.filter_poped_nodes(
+                    post_pop_sppf_nodes, sppfnode
+                )
+
+                self.add_sppf_nodes(post_pop_sppf_nodes)
+                self.add_sppf_nodes(
+                    set([SPPFNode(inner_gss_node, var_start_rsm_st, sppfnode.node)])
+                )
+
+                start_fin_set.update(sub_start_fin_set)
+
+            return start_fin_set
+
+        def pop_step() -> Set[Tuple[int, int]]:
+            new_sppf_nodes = sppfnode.gssn.pop(sppfnode.node)
+            new_sppf_nodes, start_fin_set = self.filter_poped_nodes(
+                new_sppf_nodes, sppfnode
+            )
+            self.add_sppf_nodes(new_sppf_nodes)
+            return start_fin_set
+
+        term_step()
+        res_set = var_step()
+
+        if rsm_dat.is_final:
+            res_set.update(pop_step())
+
+        return res_set
+
+    def solve_reach(
+        self,
+        from_nodes: Set[int],
+        to_nodes: Set[int],
+    ) -> Set[Tuple[int, int]]:
+        reach_set = set()
+        for snode in from_nodes:
+            gssn = self.gss.get_node(self.start_rstate, snode)
+            gssn.add_edge(RsmState(Symbol("$"), "fin"), self.accept_gssnode)
+
+            self.add_sppf_nodes(set([SPPFNode(gssn, self.start_rstate, snode)]))
+
+        while self.unprocessed != set():
+            reach_set.update(self.step(self.unprocessed.pop()))
+
+        filtered_set = set()
+        for st_fin in reach_set:
+            if st_fin[1] in to_nodes:
+                filtered_set.add(st_fin)
+        return filtered_set
 
 
 def gll_based_cfpq(
@@ -504,4 +639,5 @@ def gll_based_cfpq(
     if (final_nodes is None) or (final_nodes == set()):
         final_nodes = set(graph.nodes())
 
-    raise
+    s = GllCFPQSolver(rsm, graph)
+    return s.solve_reach(start_nodes, final_nodes)
